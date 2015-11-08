b04984efc271cb1ea511008949160dea0483add2
Stefan Brüns <stefan.bruens@rwth-aachen.de>
2015-11-08 17:40:56 +0100

    src: add classes for files/dirs/symlinks
    Signed-off-by: Stefan Brüns <stefan.bruens@rwth-aachen.de>

---
+++ src/files.cpp
@@ -0,0 +1,115 @@
+#include <fcntl.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <iostream>
+#include <boost/algorithm/string.hpp>
+#include <files.h>
+
+namespace LibFDupe {
+
+DirEnt::DirEnt(const std::string& name, const struct stat& stat, const DirEnt* parent)
+    : m_parent(parent)
+    , m_stat(stat)
+    , m_name(name)
+{
+}
+
+DirEnt::~DirEnt() {}
+
+Directory::Directory(const std::string& name, const struct stat& stat, const DirEnt* parent)
+    : DirEnt(name, stat, parent)
+{
+    if (parent) {
+        m_name = m_parent->path() + name + "/";
+    } else if (m_name.back() != '/') {
+        m_name.push_back('/');
+    }
+}
+
+File::File(const std::string& name, const struct stat& stat, const DirEnt* parent)
+    : DirEnt(name, stat, parent)
+{
+}
+
+Symlink::Symlink(const std::string& name, const struct stat& stat, const DirEnt* parent)
+    : DirEnt(name, stat, parent)
+{
+}
+
+std::string
+DirEnt::path() const
+{
+    if (m_parent)
+         return m_parent->path() + m_name;
+
+    return m_name;
+}
+
+ino_t DirEnt::inode() const
+{
+    return m_stat.st_ino;
+}
+
+dev_t DirEnt::device() const
+{
+    return m_stat.st_dev;
+}
+
+std::string
+Directory::path() const
+{
+    return m_name;
+}
+
+bool
+DirEnt::operator==(const DirEnt& other) const
+{
+    return (inode() == other.inode()) &&
+           (device() == other.device());
+}
+
+bool
+Directory::operator==(const Directory& other) const
+{
+    return (inode() == other.inode()) &&
+           (device() == other.device());
+}
+
+std::ostream& operator<<(std::ostream& stream, const DirEnt& dirent)
+{
+    dirent.dump(stream);
+    return stream;
+}
+
+void Symlink::setTarget(std::string&& target)
+{
+    m_target = target;
+}
+
+off_t File::size() const
+{
+    return m_stat.st_size;
+}
+
+inline
+void File::dump(std::ostream& stream) const
+{
+    stream << " F: " << path();
+}
+
+inline
+void Directory::dump(std::ostream& stream) const
+{
+    stream << " D:" << path();
+}
+
+inline
+void Symlink::dump(std::ostream& stream) const
+{
+    stream << " S: " << path() << " -> '" << m_target << "'";
+}
+
+
+} // namespace LibFDupe
+
+// vim: set ai expandtab shiftwidth=4
---
+++ src/files.h
@@ -0,0 +1,67 @@
+#ifndef __FDUPE_FILES_H__
+#define __FDUPE_FILES_H__
+
+#include <string>
+#include <vector>
+#include <map>
+#include <sys/stat.h>
+#include <memory>
+
+namespace LibFDupe {
+
+class DirEnt
+{
+public:
+    DirEnt(const std::string& name);
+    DirEnt(const std::string& name, const struct stat& stat, const DirEnt* parent);
+    friend std::ostream& operator<<(std::ostream& stream, const DirEnt& dirent);
+    bool operator==(const DirEnt& other) const;
+    virtual ~DirEnt();
+    virtual std::string path() const;
+    ino_t inode() const;
+    dev_t device() const;
+
+protected:
+    virtual void dump(std::ostream& stream) const = 0;
+
+    const DirEnt* m_parent;
+    struct stat m_stat;
+    std::string m_name;         //< path relative to parent
+};
+
+class Directory : public DirEnt
+{
+public:
+    Directory(const std::string& name, const struct stat& stat, const DirEnt* parent = nullptr);
+    virtual std::string path() const;
+    bool operator==(const Directory& other) const;
+
+private:
+    virtual void dump(std::ostream& stream) const;
+};
+
+class File : public DirEnt
+{
+public:
+    File(const std::string& name, const struct stat& stat, const DirEnt* parent);
+    off_t size() const;
+
+private:
+    virtual void dump(std::ostream& stream) const;
+};
+
+class Symlink : public DirEnt
+{
+public:
+    Symlink(const std::string& name, const struct stat& stat, const DirEnt* parent);
+    void setTarget(std::string&& target);
+
+private:
+    virtual void dump(std::ostream& stream) const;
+    std::string m_target;
+};
+
+} // namespace LibFDupe
+
+#endif // __FDUPE_FILES_H__
+
